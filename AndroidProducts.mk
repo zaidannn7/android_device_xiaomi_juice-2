637f0c03249fcb20233c73c312d70f1c8f1ddebd
zaidannn7 <zaidanmovic0512@gmail.com>
2022-09-22 06:35:57 +0700

    ArrowOS Bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2018-2021 ArrowOS 
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_juice.mk
+    $(LOCAL_DIR)/arrow_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_juice-user \
-    lineage_juice-userdebug \
-    lineage_juice-eng
+    arrow_juice-user \
+    arrow_juice-userdebug \
+    arrow_juice-eng
---
+++ arrow_juice.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2018-2021 ArrowOS 
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/juice/device.mk)
+
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+TARGET_INCLUDE_PIXEL_CHARGER  := true
+
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := arrow_juice
+PRODUCT_DEVICE := juice
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sm6115
+PRODUCT_MANUFACTURER := Xiaomi
+DEVICE_MAINTAINER := zaidannn7
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
