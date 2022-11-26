e06a5c5f97d83e1d030a93500d93392436a4445a
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2022-11-27 06:01:26 +0700

    juice: CRdroid/Lineage Bringup

--- lineage_juice.mk
+++ lineage_juice.mk
@@ -7,13 +7,13 @@
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := carbon_juice
+PRODUCT_NAME := lineage_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
